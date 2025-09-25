a751cdf9d5d2b7583f3c54e8ed4b5a94a0d9b769
MS3PRO <133560006+Ms3pro@users.noreply.github.com>
2025-09-25 11:35:33 +0500

    Update GPIO.ino

--- examples/GPIO/GPIO.ino
+++ examples/GPIO/GPIO.ino
@@ -5,26 +5,14 @@
 #include "MegaCAN_ExtDevice.h"
 #include "MegaCAN_RT_BroadcastHelper.h"
 #include "tables.h"
-
+TWAI_CAN::CAN;
 #include <TaskScheduler.h>
-// #define _TASK_TIMECRITICAL      // Enable monitoring scheduling overruns
-// #define _TASK_SLEEP_ON_IDLE_RUN // Enable 1 ms SLEEP_IDLE powerdowns between tasks if no callback methods were invoked during the pass
-// #define _TASK_STATUS_REQUEST    // Compile with support for StatusRequest functionality - triggering tasks on status change events in addition to time only
-// #define _TASK_WDT_IDS           // Compile with support for wdt control points and task ids
-// #define _TASK_LTS_POINTER       // Compile with support for local task storage pointer
-// #define _TASK_PRIORITY          // Support for layered scheduling priority
-// #define _TASK_MICRO_RES         // Support for microsecond resolution
-// #define _TASK_STD_FUNCTION      // Support for std::function (ESP8266 and ESP32 ONLY)
-// #define _TASK_DEBUG             // Make all methods and variables public for debug purposes
-// #define _TASK_INLINE            // Make all methods "inline" - needed to support some multi-tab, multi-file implementations
-// #define _TASK_TIMEOUT           // Support for overall task timeout
-// #define _TASK_OO_CALLBACKS      // Support for dynamic callback method binding
 
 // Required for MegaCAN library
 DECL_MEGA_CAN_REV("OpenGPIO");
 DECL_MEGA_CAN_SIG("OpenGPIO-0.1.0     ");
 
-// CAN related variables
+// TWAI_CAN related variables
 #define CAN_CS  10
 #define CAN_INT 2
 #define CAN_ID  1
@@ -70,9 +58,7 @@ setup()
 {
   setupLogging(115200);
 
-  cli();
-
-  // MCP2515 configuration
+  // TWAI configuration
   gpio.init();
   pinMode(CAN_INT, INPUT_PULLUP);// Configuring pin for CAN interrupt input
   attachInterrupt(digitalPinToInterrupt(CAN_INT), can_isr, LOW);
@@ -93,9 +79,6 @@ setup()
   // enable ADC, and request ADC complete interrupt
   ADCSRA = bit(ADEN) | bit(ADIF) | bit(ADIE);
   ADCSRA |= 0x7;// ADC prescaler 128 (Arduino default)
-
-  // enabled interrupts
-  sei();
   
   INFO("setup complete!");
 
@@ -128,7 +111,7 @@ loop()
   outPC.status0.bits.flashDataLost = gpio.flashDataLost();
   outPC.status0.bits.currFlashTable = gpio.currFlashTable();
 
-  // update CAN status registers
+  // update TWAI_CAN status registers
   outPC.canStatus = gpio.getCAN_Status();
   outPC.canErrorCount = gpio.getLogicErrorCount();
 
